cabbd6289d12572739cdd483daa2789c29cdb822
Sami <puggsoy@gmail.com>
2016-01-13 19:28:16 +1300

    Fixed output directory being the executables directory when omitted. Is now input directory as intended

--- src/Main.hx
+++ src/Main.hx
@@ -11,7 +11,7 @@ class Main
 	private var args:Array<String> = Sys.args();
 	
 	static private inline var USAGE:String = 'Usage: GFXtract <script> <inFile> [outDir]\n    script - The .gsl script containing the instructions for converting.\n    inFile - The file to convert. Can also be a directory containing the files to convert.\n    outDir - Optional, the directory to place the converted files in. If omitted, the directory of the input files is used.';
-	static private inline var VERSION:String = '0.1';
+	static private inline var VERSION:String = '0.1.1';
 	private var DATE:Date = Date.now();
 	private var ALLFILES:FILEFILTERS = { count: 1, descriptions: ['All Files (*.*)'], extensions: ['*.*'] };
 	
@@ -101,7 +101,7 @@ class Main
 	
 	private function setOutput(outPath:String)
 	{
-		outDir = (outPath == null) ? '.' : outPath;
+		outDir = (outPath == null) ? Path.directory(inputFiles[0]) : outPath;
 		
 		if (FileSystem.exists(outDir) && !FileSystem.isDirectory(outDir))
 		{
