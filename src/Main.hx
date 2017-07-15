f791152c3eca2e1e4b917c6cccdb85daf7cfa152
Sami Habib <puggsoy@gmail.com>
2017-07-15 23:38:19 +1200

    Removed support for multiline comments, they're too complex and can be added later if wanted. Also took quoted strings into accounts with single line comments

--- src/Main.hx
+++ src/Main.hx
@@ -16,22 +16,23 @@ class Main
 	private var ALLFILES:FILEFILTERS = { count: 1, descriptions: ['All Files (*.*)'], extensions: ['*.*'] };
 	
 	/**
-	 * The file containing the script.
+	 * The file containing the script
 	 */
 	private var scriptFile:FileInput;
 	
 	/**
-	 * Array of the input filenames.
+	 * Array of the input filenames
 	 */
 	private var inputFiles:Array<String>;
 	
 	/**
-	 * The output directory.
+	 * The output directory
 	 */
 	private var outDir:String;
 	
 	/**
-	 * Takes in arguments and opens script file.
+	 * Takes in arguments and begins parsing the script.
+	 * If no arguments are given then dialogue boxes appear to select the inputs
 	 */
 	public function new()
 	{
@@ -65,38 +66,35 @@ class Main
 		{
 			loadScript(args[0]);
 			loadInput(args[1]);
-			setOutput(args[2]);
+			setOutput(args[2]); //If this is null it'll be set to input directory
 		}
 		
 		parseScript(scriptFile, inputFiles, outDir);
 	}
 	
 	/**
-	 * Loads the script file.
-	 * @param	scriptPath Path of the file.
+	 * Loads the script file
+	 * @param	scriptPath
 	 */
-	private function loadScript(scriptPath:String)
+	private function loadScript(scriptPath:String):Void
 	{
-		Sys.print('- ');
 		if (!FileSystem.exists(scriptPath))
 		{
 			Sys.println('Script $scriptPath doesn\'t exist!');
 			Sys.exit(1);
 		}
 		
-		Sys.println('Loading script $scriptPath');
+		Sys.println('- Loading script $scriptPath');
 		
 		scriptFile = File.read(scriptPath);
 	}
 	
 	/**
-	 * Loads the input file/folder.
-	 * @param	inputPath Path of the file/folder.
+	 * Loads the input files/folder
+	 * @param	inputPath
 	 */
-	private function loadInput(inputPath:String)
+	private function loadInput(inputPath:String):Void
 	{
-		Sys.print('- ');
-		
 		if (!FileSystem.exists(inputPath))
 		{
 			Sys.println('Input $inputPath doesn\'t exist!');
@@ -105,7 +103,7 @@ class Main
 		
 		var type:String = (FileSystem.isDirectory(inputPath)) ? 'folder' : 'file';
 		
-		Sys.println('Loading input $type $inputPath');
+		Sys.println('- Loading input $type $inputPath');
 		
 		inputFiles = new Array<String>();
 		
@@ -113,22 +111,20 @@ class Main
 		{
 			var fileNames:Array<String> = FileSystem.readDirectory(inputPath);
 			
-			for (fn in fileNames) inputFiles.push(Path.addTrailingSlash(inputPath) + fn);
+			for (fn in fileNames) inputFiles.push(Path.join([inputPath, fn]));
 		}
 		else inputFiles.push(inputPath);
 	}
 	
-	private function setOutput(outPath:String)
+	/**
+	 * Sets the output, depending on the supplied path
+	 * @param	outPath
+	 */
+	private function setOutput(outPath:String):Void
 	{
 		outDir = (outPath == null) ? Path.directory(inputFiles[0]) : outPath;
 		if (outDir == '') outDir = '.';
 		
-		if (FileSystem.exists(outDir) && !FileSystem.isDirectory(outDir))
-		{
-			Sys.println('Output folder must be a valid directory');
-			Sys.exit(3);
-		}
-		else
 		if (!FileSystem.exists(outDir))
 		{
 			Sys.println('Output folder $outDir doesn\'t exist, do you want to create it? (y/n)');
@@ -141,29 +137,35 @@ class Main
 			else
 			{
 				Sys.println('Folder needs to exist!');
+				Sys.exit(3);
 			}
+		}
+		else
+		if (!FileSystem.isDirectory(outDir))
+		{
+			Sys.println('Output folder must be a valid directory');
+			Sys.exit(3);
 		}
 		
 		Sys.println('- Setting output directory $outDir\n');
 	}
 	
 	/**
-	 * Parses the script and runs it on each file.
-	 * @param	script  The script file.
-	 * @param	files   The array of files.
-	 * @param	outPath The output folder.
+	 * Parses the script and runs it on each file
+	 * @param	script  The script file
+	 * @param	files   The array of files
+	 * @param	outPath The output folder
 	 */
-	private function parseScript(script:FileInput, files:Array<String>, outPath:String)
+	private function parseScript(script:FileInput, files:Array<String>, outPath:String):Void
 	{
 		var lines:Array<String> = new Array<String>();
 		var inComment:Bool = false;
 		
 		while (!script.eof())
 		{
-			var r:Array<Dynamic> = removeComments(script.readLine(), inComment);
+			var line:String = removeComments(script.readLine(), lines.length + 1);
 			
-			lines.push(r[0]);
-			inComment = r[1];
+			lines.push(line);
 		}
 		
 		var lines:Array<String> = [for(line in lines) StringTools.ltrim(line)];
@@ -192,47 +194,52 @@ class Main
 	}
 	
 	/**
-	 * Removes comments from a line.
-	 * @param	line      The line to remove the comment from.
-	 * @param	inComment Whether this line starts in a multiline comment. .
-	 * @return  An array with two elements: the line without comments, and whether we are in a multiline comment.
+	 * Removes comments from a line
+	 * @param	line      The line to remove the comment from
+	 * @param	inComment Whether this line starts in a multiline comment
+	 * @return  An array with two elements: the line without comments, and whether we are in a multiline comment
 	 */
-	private function removeComments(line:String, inComment:Bool):Array<Dynamic>
+	private function removeComments(line:String, lineNum:Int):String
 	{
-		var startCom:Int = line.indexOf('#');
-		if(startCom != -1) line = line.substring(0, startCom);
-		var startCom:Int = line.indexOf('//');
-		if(startCom != -1) line = line.substring(0, startCom);
+		var inQuotes:Bool = false;
 		
-		startCom = 0;
-		var endCom:Int = -2;
-		
-		if (inComment)
+		for (i in 0...line.length)
 		{
-			endCom = line.indexOf('*/');
-		}
-		
-		while(startCom != -1)
-		{
-			if (endCom == -1)
+			var c:String = line.charAt(i);
+			
+			if (inQuotes)
 			{
-				line = line.substring(0, startCom);
-				return [line, true];
+				if (c == '"') inQuotes = false;
+				
+				continue;
 			}
 			
-			line = line.substring(0, startCom) + line.substring(endCom + 2);
+			if (c == '"')
+			{
+				inQuotes = true;
+				continue;
+			}
 			
-			startCom = line.indexOf('/*');
-			endCom = line.indexOf('*/', startCom + 2);
+			if (c == '#' || line.substr(i, 2) == '//')
+			{
+				line = line.substring(0, i);
+				break;
+			}
 		}
 		
-		return [line, false];
+		if (inQuotes)
+		{
+			Sys.println('Unclosed quotes on line $lineNum!');
+			Sys.exit(4);
+		}
+		
+		return line;
 	}
 	
 	/***************
 	 * Entry point
 	 */
-	static function main()
+	static function main():Void
 	{
 		var main:Main = new Main();
 	}
